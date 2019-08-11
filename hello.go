87acfced5c82ad187a1882f568119f1b0a580e16
sirwan <sir1afifi@gmail.com>
2019-08-11 17:12:39 +0430

    testing for loop

--- hello.go
+++ hello.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Sirwan Afifi"
-	fmt.Println("Hello" + " " + name)
-	fmt.Println(len(name))
-	fmt.Printf("%T \n", name)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	index := 1
+
+	for index < 10 {
+		fmt.Println(index)
+	}
 }
